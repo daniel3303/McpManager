ec48ae5fdf7a6fe1af44d7f864f2f4c578981f02
Daniel Oliveira <daniel-oliveira-11@hotmail.com>
2025-08-04 16:01:57 +0000

    feat(data): add design-time DbContext factory

---
+++ McpManager.Core.Data/DesignTimeApplicationDbContextFactory.cs
@@ -0,0 +1,29 @@
+using McpManager.Core.Data.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace McpManager.Core.Data;
+
+public class DesignTimeApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext> {
+    public ApplicationDbContext CreateDbContext(string[] args) {
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(GetStartupProjectPath())
+            .AddJsonFile("designsettings.json", optional: false)
+            .AddJsonFile("designsettings.Development.json", optional: true)
+            .Build();
+
+        var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+        var connectionString = configuration.GetConnectionString("ApplicationConnection");
+
+        optionsBuilder.UseSqlite(connectionString, options => {
+            options.MigrationsAssembly(GetType().Assembly);
+        });
+
+        return new ApplicationDbContext(optionsBuilder.Options);
+    }
+
+    private static string GetStartupProjectPath() {
+        return Directory.GetCurrentDirectory();
+    }
+}
