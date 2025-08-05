2e21e6ec72e1b79cdc139b009a553df94e259f8b
Daniel Oliveira <daniel-oliveira-11@hotmail.com>
2025-08-05 17:27:36 +0000

    feat(core): add custom ApplicationException

---
+++ McpManager.Core.Data/Exceptions/ApplicationException.cs
@@ -0,0 +1,24 @@
+namespace McpManager.Core.Data.Exceptions;
+
+public class ApplicationException : Exception {
+    public string Property { get; }
+    public List<string> Errors { get; }
+
+    public ApplicationException(string message) : base(message) {
+        Errors = [message];
+    }
+
+    public ApplicationException(string message, string property) : base(message) {
+        Property = property;
+        Errors = [message];
+    }
+
+    public ApplicationException(List<string> errors) : base(string.Join("; ", errors)) {
+        Errors = errors;
+    }
+
+    public ApplicationException(string property, List<string> errors) : base(string.Join("; ", errors)) {
+        Property = property;
+        Errors = errors;
+    }
+}
