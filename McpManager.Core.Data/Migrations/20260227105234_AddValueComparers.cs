532782cc4868925546dc949d7f80fc01e22aac42
Daniel Oliveira <daniel-oliveira-11@hotmail.com>
2025-08-13 07:20:25 +0000

    feat(migrations): add value comparers migration

---
+++ McpManager.Core.Data/Migrations/20260227105234_AddValueComparers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace McpManager.Core.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddValueComparers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("00000001-0000-0000-0000-000000000000"),
+                columns: new[] { "Email", "NormalizedEmail", "NormalizedUserName", "UserName" },
+                values: new object[] { "admin@mcpmanager.local", "ADMIN@MCPMANAGER.LOCAL", "ADMIN@MCPMANAGER.LOCAL", "admin@mcpmanager.local" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("00000001-0000-0000-0000-000000000000"),
+                columns: new[] { "Email", "NormalizedEmail", "NormalizedUserName", "UserName" },
+                values: new object[] { "daniel.oliveira@comudel.com", "DANIEL.OLIVEIRA@COMUDEL.COM", "DANIEL.OLIVEIRA@COMUDEL.COM", "daniel.oliveira@comudel.com" });
+        }
+    }
+}
