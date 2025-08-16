51c6f89fcf5d86a19c93d459ba8f96aedd08d43d
Daniel Oliveira <daniel-oliveira-11@hotmail.com>
2025-08-16 21:45:13 +0000

    chore(config): update default MCP connection timeout

---
+++ McpManager.Core.Data/Migrations/20260227153845_UpdateDefaultMcpConnectionTimeout.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace McpManager.Core.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateDefaultMcpConnectionTimeout : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AppSettings",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "McpConnectionTimeoutSeconds",
+                value: 120);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AppSettings",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "McpConnectionTimeoutSeconds",
+                value: 30);
+        }
+    }
+}
