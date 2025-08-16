4cf7ed13536f3bbb42eff59e24317bc9439b9d5c
Daniel Oliveira <daniel-oliveira-11@hotmail.com>
2025-08-16 14:16:25 +0000

    feat(migrations): add API key scoping and rate limiting

---
+++ McpManager.Core.Data/Migrations/20260227151719_AddApiKeyScopingRateLimitingAndAppSettings.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace McpManager.Core.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddApiKeyScopingRateLimitingAndAppSettings : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
