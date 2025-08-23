b3168f3d96685672a12be3c5cf2016144d928686
Daniel Oliveira <daniel-oliveira-11@hotmail.com>
2025-08-23 22:52:49 +0000

    feat(mcp): add service collection extensions

---
+++ McpManager.Core.Mcp/Extensions/McpServiceCollectionExtensions.cs
@@ -0,0 +1,11 @@
+using Equibles.Core.AutoWiring;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace McpManager.Core.Mcp.Extensions;
+
+public static class McpServiceCollectionExtensions {
+    public static void AddMcp(this IServiceCollection services) {
+        services.AddHttpClient();
+        services.AutoWireServicesFrom<McpAssembly>();
+    }
+}
